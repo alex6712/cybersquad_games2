51d794dc6b79e8d335c956302fbffe5b8cb25910
alex6712 <vanyukov.alex@gmail.com>
2023-07-02 21:10:36 +0300

    Добавлена документация модуля jwt

--- backend/app/jwt.py
+++ backend/app/jwt.py
@@ -11,15 +11,36 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def jwt_encode(to_encode: dict):
+def jwt_encode(to_encode: dict) -> str:
+    """
+    Кодирует переданный словарь в JWT.
+
+    :param to_encode: dict[str, str], словарь, который будет вложен в JWT
+    :return: str, JSON Web Token
+    """
     return jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
 
 
 def jwt_decode(token: Annotated[str, Depends(oauth2_scheme)]):
+    """
+    Декодирует переданный JWT в словарь.
+
+    :param token: str, JWT, из которого будет получен словарь
+    :return: dict[str, str], словарь с информацией из JWT
+    """
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 
 def create_jwt(data: dict, expires_delta: timedelta = timedelta(minutes=5)):
+    """
+    Создаёт JWT.
+
+    На вход получает информацию для кодирования и срок жизни токена.
+
+    :param data: dict[str, str], словарь с данными
+    :param expires_delta: timedelta, время жизни JWT
+    :return: str, JSON Web Token
+    """
     to_encode = data.copy()
     to_encode.update({"exp": datetime.utcnow() + expires_delta})
 

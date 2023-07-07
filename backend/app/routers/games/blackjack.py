525161b8b9b24b3b294b26041cb454d84978352f
avanyukov <vanyukov.alex@gmail.com>
2023-07-07 14:00:10 +0300

    Прикрутили CORS. Продолжается реализация игр

---
+++ backend/app/routers/games/blackjack.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import (
+    APIRouter,
+    Depends,
+    status,
+    HTTPException,
+)
+
+from app.models import APIUserModel
+from app.dependencies import get_current_user
+
+router = APIRouter(
+    prefix="/blackjack",
+    tags=["blackjack"],
+)
+
+
+@router.get("/join")
+def blackjack(user: Annotated[APIUserModel, Depends(get_current_user)]):
+    return {"hero": "gachi"}
